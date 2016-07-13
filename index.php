6cdaab8b50d99b6e7920f5fbb7ed8eb3defce51e
= <sudeep@mobilesnapp.com>
2016-07-12 22:11:44 -0700

    committing configuration and setup files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>NGINX Server</title>
+</head>
+<body>
+    <div>
+        <h1>Congratulations! NGINX-HHVM server container running successfully.</h1>
+        <h2 class="host" style=""><?php echo gethostname(); ?></h2>
+        <em>Dockbox - Open source PHP development docker environment by MobileSnapp (support@mobilesnapp.com)</em>
+    </div>
+    <?php phpinfo(); ?>
+</body>
+</html>
